3cf93be8a2cff2043d69480ea2b49cd12ac9df29
Khanh Pham <phamk0528@gmail.com>
2022-07-13 14:38:55 +0700

    Update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,26 +14,11 @@ export default class Document extends NextDocument {
                     <link rel="shortcut icon" href="/static/favicon.ico" />
                 </Head>
                 <body>
-                    <div id="fb-root"></div>
-                    <script
-                        async
-                        defer
-                        src="https://connect.facebook.net/en_GB/all.js#xfbml=1&version=v12.0&appId=253664926562060&autoLogAppEvents=1"
-                        nonce="awtmqUmg"
-                    ></script>
 
-                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-TSCGW1Y7RZ" />
 
-                    <script
-                        dangerouslySetInnerHTML={{
-                            __html: `
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-TSCGW1Y7RZ');
-`,
-                        }}
-                    />
+
+
+
                     <ColorModeScript initialColorMode="light" />
                     <Main />
                     <NextScript />
